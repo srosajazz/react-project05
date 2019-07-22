77fd5c753a3839743861d5b2e3c2e3ea98486f81
Sergio Rosa <scarlosjazz@gmail.com>
2019-07-21 22:24:50 -0400

    react-global-style

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,12 +1,6 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-  font-size: 24px;
-  color: ${props => (props.error ? 'red' : '#85bb65')};
-  font-family: Arial, Helvetica, sans-serif;
-
-  small {
-    font-size: 14px;
-    color: #333;
+    color: #FFF;
   }
 `;
